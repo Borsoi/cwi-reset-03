437cecdccd0998db00cd880efaee007fbf04dedf
Borsoi <borsoi.guilherme@gmail.com>
2021-10-15 19:33:04 -0300

    Validando as entradas e depois criando o objeto ator/diretor

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/guilhermeborsoi/services/AtorService.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/guilhermeborsoi/services/AtorService.java
@@ -21,37 +21,39 @@ public class AtorService {
     // Demais métodos da classe
 
     public void criarAtor(AtorRequest atorRequest) throws MensagemDeErro {
-            Ator ator = new Ator(fakeDatabase.recuperaAtores().size() + 1, atorRequest.getNome(), atorRequest.getDataNascimento(),
-                    atorRequest.getStatusCarreira(), atorRequest.getAnoInicioAtividade());
+
             for (Ator atores :fakeDatabase.recuperaAtores()) {
-                if (atores.getNome().equals(ator.getNome())) {
-                    String e = "Campo obrigatório não informado. Favor informar o campo nome";
+                if (atores.getNome().equals(atorRequest.getNome())) {
+                    String e = "Já existe um diretor cadastrado para o nome " + atorRequest.getNome();
                     throw new MensagemDeErro(e);
                 }
             }
-            if (ator.getNome().equals("") || ator.getNome() == null) {
+            if (atorRequest.getNome().equals("") || atorRequest.getNome() == null) {
                 String e = "Campo obrigatório não informado. Favor informar o campo nome";
                 throw new MensagemDeErro(e);
-            } else if (ator.getNome().indexOf(" ") == -1) {
+            } else if (atorRequest.getNome().indexOf(" ") == -1) {
                 String e = "Deve ser informado no mínimo nome e sobrenome para o ator";
                 throw new MensagemDeErro(e);
-            } else if (ator.getDataNascimento() == null) {
+            } else if (atorRequest.getDataNascimento() == null) {
                 String e = "Campo obrigatório não informado. Favor informar o campo data de nascimento";
                 throw new MensagemDeErro(e);
-            } else if (ator.getDataNascimento().getYear() > LocalDate.now().getYear()) {
+            } else if (atorRequest.getDataNascimento().getYear() > LocalDate.now().getYear()) {
                 String e = "Não é possível cadastrar atores não nascidos";
                 throw new MensagemDeErro(e);
-            } else if (ator.getStatusCarreira() == null) {
+            } else if (atorRequest.getStatusCarreira() == null) {
                 String e = "Campo obrigatório não informado. Favor informar o campo status carreira";
                 throw new MensagemDeErro(e);
-            } else if (ator.getAnoInicioAtividade() == null) {
+            } else if (atorRequest.getAnoInicioAtividade() == null) {
                 String e = "Campo obrigatório não informado. Favor informar o campo ano de Inicio da Atividade";
                 throw new MensagemDeErro(e);
-            } else if (ator.getAnoInicioAtividade() < ator.getDataNascimento().getYear()) {
+            } else if (atorRequest.getAnoInicioAtividade() < atorRequest.getDataNascimento().getYear()) {
                 String e = "Ano de início de atividade inválido para o ator cadastrado";
                 throw new MensagemDeErro(e);
-            } else
-                fakeDatabase.persisteAtor(ator);
+            }
+            Ator ator = new Ator(fakeDatabase.recuperaAtores().size() + 1, atorRequest.getNome(), atorRequest.getDataNascimento(),
+                atorRequest.getStatusCarreira(), atorRequest.getAnoInicioAtividade());
+
+        fakeDatabase.persisteAtor(ator);
     }
 
     public List listarAtoresEmAtividade (String filtroNome) throws MensagemDeErro {
--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/guilhermeborsoi/services/DiretorService.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/guilhermeborsoi/services/DiretorService.java
@@ -19,36 +19,38 @@ public class DiretorService {
 
     // Demais métodos da classe
 
-    public void cadastrarDiretor (DiretorRequest diretorRequest) throws MensagemDeErro {
-        Diretor diretor = new Diretor(fakeDatabase.recuperaDiretores().size() + 1, diretorRequest.getNome(),
-                diretorRequest.getDataNascimento(),diretorRequest.getAnoInicioAtividade());
-        for (Diretor diretores :fakeDatabase.recuperaDiretores()) {
-            if (diretores.getNome().equals(diretor.getNome())) {
-                String e = "Já existe um diretor cadastrado para o nome " + diretor.getNome();
+    public void cadastrarDiretor(DiretorRequest diretorRequest) throws MensagemDeErro {
+
+        for (Diretor diretores : fakeDatabase.recuperaDiretores()) {
+            if (diretores.getNome().equals(diretorRequest.getNome())) {
+                String e = "Já existe um diretor cadastrado para o nome " + diretorRequest.getNome();
                 throw new MensagemDeErro(e);
             }
         }
-        if (diretor.getNome().equals("") || diretor.getNome() == null) {
+        if (diretorRequest.getNome().equals("") || diretorRequest.getNome() == null) {
             String e = "Campo obrigatório não informado. Favor informar o campo nome";
             throw new MensagemDeErro(e);
-        } else if (diretor.getNome().indexOf(" ") == -1) {
+        } else if (diretorRequest.getNome().indexOf(" ") == -1) {
             String e = "Deve ser informado no mínimo nome e sobrenome para o ator";
             throw new MensagemDeErro(e);
-        } else if (diretor.getDataDeNascimento() == null) {
+        } else if (diretorRequest.getDataNascimento() == null) {
             String e = "Campo obrigatório não informado. Favor informar o campo data de nascimento";
             throw new MensagemDeErro(e);
-        } else if (diretor.getDataDeNascimento().getYear() > LocalDate.now().getYear()) {
+        } else if (diretorRequest.getDataNascimento().getYear() > LocalDate.now().getYear()) {
             String e = "Não é possível cadastrar atores não nascidos";
             throw new MensagemDeErro(e);
-        } else if (diretor.getAnoInicioDeAtividade() == null) {
+        } else if (diretorRequest.getAnoInicioAtividade() == null) {
             String e = "Campo obrigatório não informado. Favor informar o campo ano de Inicio da Atividade";
             throw new MensagemDeErro(e);
-        } else if (diretor.getAnoInicioDeAtividade() < diretor.getDataDeNascimento().getYear()) {
+        } else if (diretorRequest.getAnoInicioAtividade() < diretorRequest.getDataNascimento().getYear()) {
             String e = "Ano de início de atividade inválido para o ator cadastrado";
             throw new MensagemDeErro(e);
-        } else
-            fakeDatabase.persisteDiretor(diretor);
+        }
+
+        Diretor diretor = new Diretor(fakeDatabase.recuperaDiretores().size() + 1, diretorRequest.getNome(),
+            diretorRequest.getDataNascimento(), diretorRequest.getAnoInicioAtividade());
     }
+
 
     public List listarDiretores (String filtroNome) throws MensagemDeErro {
         if (fakeDatabase.recuperaDiretores().isEmpty()) {
